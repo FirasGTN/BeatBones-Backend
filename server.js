98a36c9acfb0e871701c68aaef7eab2e0706b717
firas <firasazzebi66@gmail.com>
2023-12-08 00:28:59 +0100

    Beat Bones Backend

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ require("dotenv").config();
 const DB = process.env.DB;
 // `mongodb+srv://firasazzebi66:${DB}@cluster0.z3mogai.mongodb.net/beatbones?retryWrites=true&w=majority`
 mongoose
-  .connect(
-    "mongodb+srv://BeatBones:Firas134679@beatbones.dlibg6b.mongodb.net/Test?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+.connect(
+    "mongodb+srv://BeatBones:Firas134679@beatbones.dlibg6b.mongodb.net/Test?retryWrites=true&w=majority"
   )
   .then(() => console.log("connected to the database"))
   .catch((err) => console.log(err));
