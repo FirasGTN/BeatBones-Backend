2436667a2ed2c1b1f4aa8ac8b03c828c0696f633
firas <firasazzebi66@gmail.com>
2023-12-07 21:20:16 +0100

    Beat Bones Backende

--- Routes/Admin/index.js
+++ Routes/Admin/index.js
@@ -4,7 +4,6 @@ const route = express.Router();
 route.post("/account/addproduct", require("./addProduct"));
 route.put("/updateProduct", require("./updateproduct"));
 route.delete("/deleteProduct/:deleteProductId", require("./deleteProduct"));
-route.get("/users", require("./getUsers"));
 route.put("/banuser", require("./banUser"));
 
 module.exports = route;
